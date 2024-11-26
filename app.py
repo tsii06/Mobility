49fa38289463b2c2e007683fec071627d9a8bfdb
tsii06 <tsiorydrage@gmail.com>
2024-11-26 11:38:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,6 @@ detail_commune_callback(app)
 
 server = app.server
 if __name__ == '__main__':
-    # get_resource_usage()
-    app.run_server(debug=True)
+    # Récupérer le port depuis la variable d'environnement ou utiliser 8050 par défaut
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host="0.0.0.0", port=port)
